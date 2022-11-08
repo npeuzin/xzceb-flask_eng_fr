0743862f711d93e28ecc77a14ccd66b9736ad607
npeuzin <npeuzin.free@gmail.com>
2022-11-08 06:15:25 +0100

    Fianl changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,25 @@
+import os
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-
-app = Flask("Web Translator")
-
+server_dir = "C:\\Users\\Nicolas\\Documents\\GitHub\\testrepo\\xzceb-flask_eng_fr\\final_project"
+app = Flask("Web Translator", template_folder = server_dir + "\\templates", static_folder= server_dir + "\\static")
+app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    result = translator.english_to_french(textToTranslate)
+    return result
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    result = translator.french_to_english(textToTranslate)
+    return result
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
